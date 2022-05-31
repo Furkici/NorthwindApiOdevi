89e88a2c94643a7cc89080cda2d5e594e161e7f9
semih güneş <furkansemihgunes@hotmail.com>
2022-05-31 23:50:33 +0300

    as

---
+++ NorthwindApi/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NorthwindApi.Models;
+
+namespace NorthwindApi.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(db.Customers.ToList());
+        }
+
+        public HttpResponseMessage GetCustomers(string id)
+        {
+            Customer customer = db.Customers.FirstOrDefault(x => x.CustomerID == id);
+            if (customer != null)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, customer);
+                return response;
+            }
+            else
+            {
+                HttpResponseMessage errorResponse = Request.CreateErrorResponse(HttpStatusCode.NotFound, "Üzgünüz Müşteri Bulunamadı ");
+                return errorResponse;
+            }
+
+
+        }
+    }
+}
---
+++ NorthwindApi/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NorthwindApi.Models;
+
+namespace NorthwindApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public IHttpActionResult GetOrders()
+        {
+            return Ok(db.Orders.ToList());
+        }
+        public HttpResponseMessage GetOrders(int id)
+        {
+            Order orders = db.Orders.FirstOrDefault(x => x.OrderID == id);
+            if (orders != null)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, orders);
+                return response;
+            }
+            else
+            {
+                HttpResponseMessage errorResponse = Request.CreateErrorResponse(HttpStatusCode.NotFound, "Üzgünüz Sipariş Bulunamadı ");
+                return errorResponse;
+            }
+        }
+    }
+}
